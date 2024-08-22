d1af9decb5b4761fca8fffcb0ba690d16b12798f
jobverplanke <job.verplanke@gmail.com>
2024-08-22 17:29:29 +0200

    untrack main.tf

--- main.tf
+++
@@ -1,6 +0,0 @@
-module "rds" {
-  # https://developer.hashicorp.com/terraform/language/modules/sources#modules-in-package-sub-directories
-  source = "./modules/rds"
-
-  client_name = "Kaas_Boer 1991 bami Blok-special"
-}
