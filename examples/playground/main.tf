03d2bca3ecb022f4d95c783216462a0b203d92f2
jobverplanke <job.verplanke@gmail.com>
2024-11-01 13:03:57 +0100

    added playground to examples

---
+++ examples/playground/main.tf
@@ -0,0 +1,8 @@
+locals {
+  client_name = "Test Client Name"
+  client_id = lower(join("", regexall("[A-Z]", local.client_name))) # should output: TCN
+}
+
+output "client_id" {
+  value = local.client_id
+}
