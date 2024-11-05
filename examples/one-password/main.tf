acec775352ad7350de3da3b66fa9efa684b1c5ab
jobverplanke <job.verplanke@gmail.com>
2024-11-05 07:28:39 +0100

    updated 1Password example

--- examples/one-password/main.tf
+++ examples/one-password/main.tf
@@ -1,18 +1,45 @@
 module "one-password" {
   source = "../../modules/one-password"
 
-  service_account_token = ""
+  service_account_token = var.service_account_token
 
-  client_name = "Test Client"
+  vault = "DevOps Tech"
 
-  title    = "Test MySQL User"
-  category = "database"
-  vault    = "DevOps Tech"
+  title                    = "Test MySQL User"
+  category                 = "login"
+  should_generate_password = true
 
-  database_name = module.mysql.database
-  database_type = "mysql"
-  hostname      = module.mysql.host
-  port          = module.mysql.port
-  username      = module.mysql.user
-  password      = module.mysql.password
+  # database_name = "database name"
+  # database_type = "mysql"
+  # hostname = "some hostname"
+  # port = "3306"
+  # url = "some url"
+
+  section = [
+    {
+      section_label = "Access Keys"
+      fields = [
+        {
+          label = "Access Key"
+          type  = "STRING"
+          value = "SomeAccessKey"
+        },
+        {
+          label = "Access Key Secret"
+          type  = "CONCEALED"
+          value = "SomeAccessKeySecret"
+        }
+      ]
+    },
+    {
+      section_label = "More random things"
+      fields = [
+        {
+          label = "AA random label"
+          type  = "STRING"
+          value = "RandomValue"
+        },
+      ]
+    }
+  ]
 }
